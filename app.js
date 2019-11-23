9bcc10d2e67c3d6437ae376d060351094f84eb79
duai <duaifzn@gmail.com>
2019-11-23 12:56:35 +0800

    add mongodb connect

--- app.js
+++ app.js
@@ -2,36 +2,62 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+
 const exphbs = require('express-handlebars')
-const resList = require('./restaurant.json')
+//const resList = require('./restaurant.json')
 //add a template engine
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 //set template engine
 app.set('view engine', 'handlebars')
 //set static file
 app.use(express.static('public'))
+//connect mongodb
+mongoose.connect('mongodb://localhost/resList', { useNewUrlParser: true, useUnifiedTopology: true })
+//return 'connection' when connect mongodb
+const db = mongoose.connection
+//set db status
+db.on('error', () => {
+    console.log('connect error')
+})
+db.once('open', () => {
+    console.log('mongodb connected')
+})
 
+const resList = require('./models/resList.js')
 
-app.get('/',(req,res)=>{
-    res.render('index',{restaurant: resList.results})
+app.get('/', (req, res) => {
+    resList.find((err, reslists) => {
+        if (err) console.error(err)
+        return res.render('index', { restaurant: reslists })
+    })
+
     //res.send(`server is runing`)
 })
-app.get('/restaurants/:id',(req,res)=>{
-    //console.log(req)
-    const restaurant = resList.results.find(hotel => hotel.id === Number(req.params.id))
-    res.render('show',{restaurant: restaurant})
+app.get('/restaurants/:id', (req, res) => {
+    // console.log(req)
+    resList.find({ id: req.params.id }, (err, reslists) => {
+        if (err) return console.error(err)
+        //console.log(reslists.name)
+        return res.render('show', { restaurant: reslists[0] })
+    })
 })
 
-app.get('/search',(req,res)=>{
-    //console.log(req.query.keyword)
-    const keyword = req.query.keyword
-    const restaurant = resList.results.filter(hotel => hotel.name.toLowerCase().includes(req.query.keyword.toLowerCase()))
-    res.render('index',{restaurant: restaurant, keyword: keyword})
+app.get('/search', (req, res) => {
+    //搜尋的內容正規化
+    const keyword = new RegExp(req.query.keyword)
+    //const restaurant = resList.results.filter(hotel => hotel.name.toLowerCase().includes(req.query.keyword.toLowerCase()))
+    resList.find({ name: keyword }, (err, reslists) => {
+        if (err) return console.error(err)
+        console.log(reslists)
+        return res.render('index', { restaurant: reslists, keyword: req.query.keyword })
+    })
+    //res.render('index', { restaurant: restaurant, keyword: keyword })
 })
 
 
 
 //start and listen server
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Express is listening on localhost:${port}`)
 })
---
+++ seeds/ResListseed.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const resList = require('../models/resList')
+const jsonList = require('../restaurant.json')
+//connect mongodb
+mongoose.connect('mongodb://localhost/resList', { useNewUrlParser: true, useUnifiedTopology: true })
+//return 'connection' when connect mongodb
+const db = mongoose.connection
+//db status
+db.on('error', () => {
+  console.log('connect error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+
+  //add data to 'resList' database
+  for (let result in jsonList.results) {
+    //console.log(jsonList.results[result].id)
+    resList.create(
+      {
+        id: jsonList.results[result].id,
+        name: jsonList.results[result].name,
+        name_en: jsonList.results[result].name_en,
+        category: jsonList.results[result].category,
+        image: jsonList.results[result].image,
+        location: jsonList.results[result].location,
+        phone: jsonList.results[result].phone,
+        google_map: jsonList.results[result].google_map,
+        rating: jsonList.results[result].rating,
+        description: jsonList.results[result].description
+      }
+    )
+  }
+  console.log('seed done')
+
+
+})
